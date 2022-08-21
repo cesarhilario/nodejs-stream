f512fd013e51de59a690d44e00c95112b4b62ff3
César Hilário <cesar.hilario.dev@gmail.com>
2022-08-21 00:25:53 -0300

    test: GET /home - should respond with home/index.html a file stream

--- spotify_radio/tests/unit/server/routes.test.js
+++ spotify_radio/tests/unit/server/routes.test.js
@@ -2,7 +2,7 @@ import { jest, expect, describe, test, beforeEach } from '@jest/globals';
 import config from '../../../server/config.js';
 import { Controller } from '../../../server/controller.js';
 
-import { handler } from '../../../server/routes';
+import { handler } from '../../../server/routes.js';
 import TestUtil from '../_util/testUtil.js';
 
 const {
@@ -29,7 +29,23 @@ describe('#Routes - test suite for api response', () => {
     expect(params.response.end).toHaveBeenCalled();
   });
 
-  test.todo(`GET /home - should respond with ${homeHTML} a file stream`);
+  test(`GET /home - should respond with ${homeHTML} a file stream`, async () => {
+    const params = TestUtil.defaultHandleParams();
+    params.request.method = 'GET';
+    params.request.url = '/home';
+    const mockFileStream = TestUtil.generateReadableStream(['data']);
+
+    jest.spyOn(Controller.prototype, Controller.prototype.getFileStream.name).mockResolvedValue({
+      stream: mockFileStream,
+    });
+    jest.spyOn(mockFileStream, 'pipe').mockReturnValue();
+
+    await handler(...params.values());
+
+    expect(Controller.prototype.getFileStream).toBeCalledWith(homeHTML);
+    expect(mockFileStream.pipe).toBeCalledWith(params.response);
+  });
+
   test.todo(`GET /controller - should respond with ${controllerHTML} a file stream`);
   test.todo(`GET /file.ext - should respond with a file stream`);
   test.todo(`GET /unknown - given an inexistent route, it should respond with 404`);
